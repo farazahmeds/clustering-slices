9071419ef95049ba8939bc470e7225318a378e26
Faraz Nizamani <53582614+farazahmeds@users.noreply.github.com>
2021-11-02 17:15:13 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+'''Cluster slices within NIFTI based on perceptual similarity or Structural Similarity Index Measure'''
+
+from clustering import PerceptualSimilarityClustering
+from clustering import SSIMClustering
+
+def main():
+
+    k = PerceptualSimilarityClustering('data/BraTS19_2013_2_1_t1.nii.gz', 5) #k=5
+    k = k.return_samples()
+
+
+if __name__ == '__main__':
+    main()
